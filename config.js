61ee3b12caffeb1e0248c7c07282e4f6c80cd467
𝙅𝙐𝞜𝞓𝞘𝘿 𝟲𝟴𝟳🪀 <162732210+Junidjamshad@users.noreply.github.com>
2024-04-08 09:48:01 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,11 +7,11 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.ownername = "*•|✺͜͡➳⭗⎼ͥ͢⎼⎼ͣ⎼⎼ͫ𝑱𝑼𝑵𝑨𝑰𝑫*•";
+global.owner = ['923272213933'];
+global.premium = ['923272213933'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.author = "*•|✺͜͡➳⭗⎼ͥ͢⎼⎼ͣ⎼⎼ͫ𝑱𝑼𝑵𝑨𝑰𝑫*•";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
